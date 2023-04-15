d9482f1ec86a200f0a476799800e8fd8bb14c596
depler <depler.mv@gmail.com>
2023-04-15 17:37:53 +0300

    code

--- lib/libtransmission/version.h
+++ lib/libtransmission/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PEERID_PREFIX             "-TR4020-"
-#define USERAGENT_PREFIX          "4.0.2"
-#define SHORT_VERSION_STRING      "4.0.2"
-#define LONG_VERSION_STRING       "4.0.2 (build 2023-03-16)"
+#define PEERID_PREFIX             "-TR4030-"
+#define USERAGENT_PREFIX          "4.0.3"
+#define SHORT_VERSION_STRING      "4.0.3"
+#define LONG_VERSION_STRING       "4.0.3 (build 2023-04-15)"
